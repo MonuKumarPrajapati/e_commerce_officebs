0e899a4f3531173a0d114d1351ba388f5a037719
Monu <monuprajapati3882@gmail.com>
2024-09-26 16:41:42 +0530

    js added for otp

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const  otpInputs = document.querySelectorAll('.form-control')
+
+    otpInputs.forEach((input, index) => {
+        input.addEventListener('input', () => {
+            if (input.value.length === 1) {
+                const nextInput = otpInputs[index + 1];
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            }
+        })
+    })
+
+
+ 
